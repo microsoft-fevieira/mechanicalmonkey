01ed6eb32da7fd401f05353d6f080456c82eb0f9
drMJ <mihaijal@microsoft.com>
2021-10-23 17:22:29 -0700

    test bug fix

--- test/test_env.py
+++ test/test_env.py
@@ -2,13 +2,14 @@ import random
 import mmky
 from mmky.tasks.stack.stacksim import StackSim
 from mmky.tasks.stack.stackreal import StackReal
-
+import os
 
 
 if __name__ == '__main__':
-    env = mmky.env.RoboSuiteEnv(StackSim, StackReal, config="C:\\code\\mechanicalmonkey\\test\\test_config.yaml")
+    cfg_file = os.path.join(os.path.dirname(__file__), 'test_config.yaml')
+    env = mmky.env.RoboSuiteEnv(StackSim, StackReal, config=cfg_file)
     env.reset()
 
-    for i in range(1000):
+    for i in range(100):
         env.step([random.random()-0.5,0,0,0,0,0,0])
 
